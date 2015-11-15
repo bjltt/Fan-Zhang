2156c174b1bde6b84197010282dda91257eb7b63
Fan Zhang <leonakim55@yahoo.ie>
2015-11-15 14:56:45 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,20 @@
         @user = User.find(params[:id])
       end
 
-      def new
+     def new
+        @user = User.new
       end
-    end
+
+       def create
+        secure_params = params.require(:user).permit(:name, :email, 
+                                  :password, :password_confirmation)
+        @user = User.new(secure_params)
+        if @user.save
+          flash[:success] = "Welcome to the Twitter App!" 
+           redirect_to @user# Handle a successful save.
+        else
+            render 'new'# Handle an unsuccessful save.     
+        end
+      end
+
+   end
