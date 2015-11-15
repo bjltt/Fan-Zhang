09601d12389c0df2b1086a7ed49851536bab9fac
Fan Zhang <leonakim55@yahoo.ie>
2015-11-15 17:29:14 +0000

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@
                                   :password, :password_confirmation)
         @user = User.new(secure_params)
         if @user.save
+          remember @user 
           flash[:success] = "Welcome to the Twitter App!" 
            redirect_to @user# Handle a successful save.
         else
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
           get 'contact' => 'static_pages#contact'
           get 'signup'  => 'users#new'
           get 'users/new'
+          get    'login'   => 'sessions#new'
+      post   'login'   => 'sessions#create'
+      delete 'logout'  => 'sessions#destroy'
 
       end
